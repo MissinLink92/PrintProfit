6a6ad0a48b8e49464e710602f09432a06b5b0f06
MissinLink92 <connorsjones92@hotmail.com>
2026-09-21 19:46:58 +0100

    Add shared printer price data

---
+++ printer-price-data.js
@@ -0,0 +1,15 @@
+(()=>{
+'use strict';
+// Single source of truth for printer prices that are also shown in Price Finder.
+// Prices are GBP snapshots and should be refreshed when Price Finder data is refreshed.
+window.PRINTPROFIT_PRINTER_PRICES=Object.freeze({
+  'A1 mini':169.00,
+  'A1':259.00,
+  'P1S':429.00,
+  'Neptune 4':159.00,
+  'Ender-3 V3 SE':149.00,
+  'Ender-3 V3 KE':219.00,
+  'K1C':379.00,
+  'Adventurer 5M':209.00
+});
+})();
